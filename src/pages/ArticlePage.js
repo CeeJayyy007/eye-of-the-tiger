529cc200458b1f1d40d0d8d9b4beaf4c5ee9ec6a
CeeJayyy007 <63643138+CeeJayyy007@users.noreply.github.com>
2023-02-15 11:41:53 -0500

    add token and authorization to article page component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -19,19 +19,30 @@ const ArticlePage = () => {
 
   useEffect(() => {
     const fetchArticleInfoData = async () => {
-      const response = await axios.get(`/api/articles/${articleId}`);
+      const token = user && (await user.getIdToken());
+      const headers = token ? { authtoken: token } : {};
+      const response = await axios.get(`/api/articles/${articleId}`, {
+        headers,
+      });
       const newArticleInfo = response.data;
       setArticleInfo(newArticleInfo);
     };
     fetchArticleInfoData();
-  }, [articleId]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const article = articles?.find((article) => article.name === articleId);
 
   const addUpvote = async () => {
-    const response = await axios.put(`/api/articles/${articleId}/upvote`);
-    const newArticleInfo = response.data;
-    setArticleInfo(newArticleInfo);
+    const token = user && (await user.getIdToken());
+    const headers = token ? { authtoken: token } : {};
+    const response = await axios.put(
+      `/api/articles/${articleId}/upvote`,
+      null,
+      { headers }
+    );
+    const updatedArticle = response.data;
+    setArticleInfo(updatedArticle);
   };
 
   const navigateToLogin = () => {
